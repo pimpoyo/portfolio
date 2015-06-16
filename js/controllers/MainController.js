2d32039472e00eba32a04488c255f93e99af8cae
Juan Arias <pimpoyolega@gmail.com>
2015-06-16 08:33:01 +0200

    Add first angular files

---
+++ js/controllers/MainController.js
@@ -0,0 +1,14 @@
+app.controller('MainController', ['$scope',
+function($scope){
+	$scope.images = [{
+		image: 'img/IMG_4556.jpg',
+		className: 'all studio',
+		text: 'Texto para el alt'
+	},
+	{
+		image: 'img/IMG_4851.jpg',
+		className: 'all landscape',
+		text: 'Texto para el alt'
+	}];	
+	
+}]);
