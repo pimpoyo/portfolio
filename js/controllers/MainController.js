f1afe322bb302df34c365fc7d3b165c4fec5cf16
pimpoyo <pimpoyolega@gmail.com>
2015-06-22 21:47:31 +0200

    Modal (WIP)

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,70 +1,82 @@
 app.controller('MainController', ['$scope',
 function($scope){
 	$scope.images = [{
+		id: '1',
 		image: 'img/cartel_min.jpg',
 		className: '[all, photoshop]',
 		text: 'Texto para el alt'
 	},
 	{
+		id: '2',
 		image: 'img/zapatilla_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '3',
 		image: 'img/navidad_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '4',
 		image: 'img/modelo_min.jpg',
 		className: '[all, photoshop]',
 		text: 'Texto para el alt'
 	},
 	{
+		id: '5',
 		image: 'img/autorretrato_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '6',
 		image: 'img/mundial_min.jpg',
         className: '[all, photoshop]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '7',
 		image: 'img/portArt_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	},
     {
+		id: '8',
 		image: 'img/nombre_min.jpg',
         className: '[all, photoshop]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '9',
 		image: 'img/reloj_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '10',
 		image: 'img/vino_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	},
     {
+		id: '11',
 		image: 'img/ella_min.jpg',
         className: '[all, photoshop]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '12',
 		image: 'img/motor_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	},
 	{
+		id: '13',
 		image: 'img/pimiento_min.jpg',
         className: '[all, illustrator]',
         text: 'Texto para el alt'
 	}
-
 ];	
 }]);
